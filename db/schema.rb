e116752ca16e7ae8da04a23a211aaf3e9aa31b31
pulgamecanica <pulgamecanica11@gmail.com>
2022-03-23 23:46:02 +0000

    Postgre Setup

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_03_23_155248) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -31,7 +34,7 @@ ActiveRecord::Schema.define(version: 2022_03_23_155248) do
     t.string "subject"
     t.string "reservation_code"
     t.string "email"
-    t.integer "room_id", null: false
+    t.bigint "room_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_reservations_on_room_id"
