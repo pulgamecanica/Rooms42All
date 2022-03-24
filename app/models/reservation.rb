c28879cacbaaea86407b2517aaa6672a353a8a6d
pulgamecanica <pulgamecanica11@gmail.com>
2022-03-24 12:57:58 +0000

    Validations Work

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,30 @@
 class Reservation < ApplicationRecord
   belongs_to :room
+  validates :start, presence: true
+  validates :end, presence: true
+  validates :attendees, presence: true
+  validates :subject, presence: true
+  validates :email, presence: true
+  validate :validate_attendees, :validate_date, :validate_email
+
+  def validate_attendees
+    if (attendees < 1)
+      errors.add(:attendees, "has to be bigger then 0!")
+    end
+  end
+
+  def validate_date
+    if (start >= self.end)
+      errors.add(:start, "has to end after it has sarted")
+    end
+    if (start <= DateTime.now)
+      errors.add(:start, "has to be set for after the current time")
+    end
+  end
+
+  def validate_email
+    if not email.include? "@"
+      errors.add(:email, "is invalid")
+    end
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,11 @@ class Room < ApplicationRecord
 	validates :capacity, presence: true
 	validates :name, presence: true
 	validates :location, presence: true
+	validate :validate_capacity
+
+	def validate_capacity
+		if (capacity < 1)
+			errors.add(:capacity, "has to be bigger then 0!")
+		end
+	end
 end
